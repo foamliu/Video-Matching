edb23e37294ce2bbb71bc258aef79b985f0a1736
Yang Liu <liuyang12@focusmedia.cn>
2019-04-18 16:17:01 +0800

    update

--- detect.py
+++ detect.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     mat = np.load('video.npy')
@@ -11,3 +13,17 @@ if __name__ == "__main__":
     maximum = ret[index]
     print(index)
     print(maximum)
+
+    threshold = 43.12986168973048
+    cosine = np.clip(maximum, -1, 1)
+    theta = math.acos(cosine)
+    theta = theta * 180 / math.pi
+    print(theta)
+    print(theta < threshold)
+
+    fps = 25.
+    time = 1/fps*index
+    print(time)
+
+    plt.plot(ret)
+    plt.show()

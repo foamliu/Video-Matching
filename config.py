e257b155b390c166a9f13640ee6c747b4618fc9d
Yang Liu <liuyang12@focusmedia.cn>
2019-05-27 17:04:34 +0800

    update

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
-print('device: ' + str(device))
+# print('device: ' + str(device))
 
 im_size = 224
 # Data parameters
 num_classes = 9935
+
+pickle_file = ''
---
+++ gen_features.py
@@ -0,0 +1,77 @@
+import os
+import pickle
+
+import cv2 as cv
+import numpy as np
+import torch
+from PIL import Image
+from torchvision import transforms
+from tqdm import tqdm
+
+from config import device
+from config import im_size
+
+
+def get_image(img):
+    img = img[..., ::-1]  # RGB
+    img = Image.fromarray(img, 'RGB')  # RGB
+    img = transformer(img)
+    return img.to(device)
+
+
+if __name__ == "__main__":
+    checkpoint = 'BEST_checkpoint.tar'
+    print('loading model: {}...'.format(checkpoint))
+    checkpoint = torch.load(checkpoint)
+    model = checkpoint['model'].to(device)
+    model.eval()
+
+    data_transforms = {
+        'val': transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    transformer = data_transforms['val']
+
+    folder = 'video'
+    files = [f for f in os.listdir(folder) if f.endswith('.mp4')]
+    frames = []
+
+    for file in tqdm(files):
+        filename = os.path.join(folder, file)
+        file = file[3:]
+        tokens = file.split('-')
+        name = tokens[0] + '-' + tokens[1]
+        # print(name)
+
+        cap = cv.VideoCapture(filename)
+        fps = cap.get(cv.CAP_PROP_FPS)
+
+        frame_idx = 0
+        while cap.isOpened():
+            success, frame = cap.read()
+            if success:
+                frame_info = dict()
+                frame_info['name'] = name
+                frame_info['idx'] = frame_idx
+                frame_info['fps'] = fps
+
+                frame = cv.resize(frame, (im_size, im_size))
+                img = get_image(frame)
+                imgs = torch.zeros([1, 3, 112, 112], dtype=torch.float)
+                imgs[0] = img
+                with torch.no_grad():
+                    output = model(imgs)
+                    feature = output[0].cpu().numpy()
+                    feature = feature / np.linalg.norm(feature)
+                    frame_info['feature'] = feature
+
+                    frame_idx += 1
+
+    with open('video_index.pkl', 'wb') as file:
+        pickle.dump(frames, file)
+
+    num_frames = len(frames)
+    print('num_frames: ' + str(num_frames))
